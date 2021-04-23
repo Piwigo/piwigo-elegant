33f845423aa836135e622d131df8d8de1e930e3c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-04-23 10:08:59 +0200

    Update eu_ES, thanks to bixerdo

---
+++ language/eu_ES/theme.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Comments Panel'] = 'Iruzkinen panela';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Aukeratu zein izan beharko litzatekeen panel bakoitzeko lehenetsitako egoera, edo animazioa ezgaitu:';
+$lang['Disable the animation'] = 'Ezgaitu animazioa';
+$lang['Elegant, Configuration Page'] = 'Elegant, konfigurazio orria';
+$lang['Hidden'] = 'Ezkutatuta';
+$lang['Main Menu Panel'] = 'Menu nagusiaren panela';
+$lang['Displayed'] = 'Bistaratuta';
+$lang['Panels options'] = 'Panelen aukerak';
+$lang['Photo Description Panel'] = 'Argazkien deskribapenen panela';
