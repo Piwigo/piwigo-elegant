f15abe9daf7b07bd1c3ef1121c59415b405f1ffc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-08 06:06:25 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/theme.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Veldu hvað ætti að vera sjálfgefið ástand fyrir hvert borð, eða slökktu á hreyfimyndinni:';
+$lang['Comments Panel'] = 'Umsagnaborð';
+$lang['Disable the animation'] = 'Afvirkja hreyfimyndina';
+$lang['Displayed'] = 'Sýnd';
+$lang['Elegant, Configuration Page'] = 'Glæsileg, stillingarsíða';
+$lang['Hidden'] = 'Falin';
+$lang['Main Menu Panel'] = 'Aðalvalmyndaborð';
+$lang['Panels options'] = 'Borðmöguleikar';
+$lang['Photo Description Panel'] = 'Myndlýsingaborð';
