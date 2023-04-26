48ca44e2f9e11cd60467a9f66c5e154d0556a3cb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-04-26 16:43:11 +0200

    Update es_MX, thanks to grantiago

---
+++ language/es_MX/theme.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Elegant, Configuration Page'] = 'Página de configuración elegante';
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Elija cuál debería ser el estado predeterminado para cada panel, o deshabilite la animación:';
+$lang['Comments Panel'] = 'Panel de comentarios';
+$lang['Disable the animation'] = 'Deshabilitar la animación';
+$lang['Displayed'] = 'Desplegado';
+$lang['Hidden'] = 'Oculto';
+$lang['Main Menu Panel'] = 'Panel del menú principal';
+$lang['Panels options'] = 'Opciones de paneles';
+$lang['Photo Description Panel'] = 'Foto Descripción Panel';
