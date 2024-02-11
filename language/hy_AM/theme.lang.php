bb9b5a09714493757be760ff322ebf791d12954d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-11 15:50:53 +0100

    Update hy_AM, thanks to gayane

---
+++ language/hy_AM/theme.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Choose what should be the default state for each panel, or disable the animation:'] = 'Ընտրել, թե ինչ պետք է լինի default վիճակը յուրաքանչյուր վահանակի համար կամ անջատել անիմացիան';
+$lang['Comments Panel'] = 'Մեկնաբանությունների վահանակ';
+$lang['Disable the animation'] = 'Անջատել անիմացիաները';
+$lang['Displayed'] = 'Ցուցադրված է';
+$lang['Elegant, Configuration Page'] = 'Elegant, կազմաձևման էջ';
+$lang['Hidden'] = 'Թաքնված';
+$lang['Main Menu Panel'] = 'Հիմնական մենյուի վահանակ';
+$lang['Panels options'] = 'Վահանակների ընտրանքներ';
+$lang['Photo Description Panel'] = 'Լուսանկարի նկարագրության վահանակ';
